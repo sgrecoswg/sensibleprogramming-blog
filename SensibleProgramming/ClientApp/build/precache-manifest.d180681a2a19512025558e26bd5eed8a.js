717957edcb791cc3f3800ab90743e86d4350ccdf
sgrecoswg <sgrecoswg@gmail.com>
2021-12-29 14:14:33 -0500

    publish

---
+++ SensibleProgramming/ClientApp/build/precache-manifest.d180681a2a19512025558e26bd5eed8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85e679f8f7139a41eac366f6a85d2a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1507c235b93cc683053",
+    "url": "/static/css/2.8cc8eaf9.chunk.css"
+  },
+  {
+    "revision": "751212715169bdcd2268",
+    "url": "/static/css/main.c6d14ce2.chunk.css"
+  },
+  {
+    "revision": "d1507c235b93cc683053",
+    "url": "/static/js/2.e8aa064f.chunk.js"
+  },
+  {
+    "revision": "27bd8d6c4e5cbf0bc4c59ba46130a6d2",
+    "url": "/static/js/2.e8aa064f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751212715169bdcd2268",
+    "url": "/static/js/main.075a3309.chunk.js"
+  },
+  {
+    "revision": "2ec04e2afccbfdb66971",
+    "url": "/static/js/runtime-main.f01b7f8b.js"
+  }
+]);
